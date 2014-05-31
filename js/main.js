5140ef64bc1c9bd5f9927baca39c301bce2f1a53
Aimee <AimeeKnight83@gmail.com>
2014-05-31 09:38:56 -0500

    calculate discounts for multi child

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
   function initialize(){
     $(document).foundation();
     $("#submit").click(buildUrl);
-    $("input, select").change(calculateVoiceWorkshopTotal);
+    $("input, select").change(splitPayments)
   }
 
   function getNumberofKids(){
@@ -17,21 +17,54 @@
     return qty;
   }
 
-  function calculateVoiceWorkshopTotal(){
-    getNumberofKids();
-    var $total = 0;
-    var $workshopDescription = $("select#voice-option > option:selected").text();
-    var $qty = $("select#voice-option-qty > option:selected").text();
-    if (($qty * 1) >= 2 && $workshopDescription !== ""){
-      var total = $qty * 100;
+  function calculateVoiceDiscount(){
+    var qty = getNumberofKids();
+    var total = 0;
+    var $voiceOption = $("select#voice-option > option:selected").text();
+    if (qty >= 2 && $voiceOption !== ""){
+      total = 100;
       var discount = total * .10;
       total -= discount;
-    } else if ($qty === "1") {
-      total = 100;
-    }else {
+    } else if (qty == 1 && $voiceOption !== ""){
+      total += 100
+    } else {
       total = 0
     }
-    $("#total").text(total);
+    return total;
+  }
+
+  function calculateHomeSchoolDiscount(){
+    var qty = getNumberofKids();
+    var total = 0;
+    var $homeSchool = $("input[name=home_school]").is(":checked")
+    var $kidCount = $("select#child-qty > option:selected").text();
+    if (qty >= 2 && $kidCount !== "" && $homeSchool){
+      total = 100;
+    } else if (qty === 1 && $homeSchool) {
+      total = 130;
+    } else {
+      total = 0;
+    }
+    return total;
+  }
+
+  function splitPayments(){
+    var $paymentOption = $("#payment-option").find(":selected").val();
+    var payment = calculateTotal();
+    if ($paymentOption === "pay_deposit"){
+      payment = Math.ceil(payment / 3);
+    }
+    $("#total").text(payment);
+    return payment;
+  }
+
+  function calculateTotal(){
+    var total = 0;
+    var homeSchoolTotal = calculateHomeSchoolDiscount();
+    var voiceTotal = calculateVoiceDiscount();
+    total += homeSchoolTotal;
+    total += voiceTotal;
+    console.log(total);
     return total;
   }
 
@@ -40,38 +73,39 @@
     var $name = $("input[name=name]").val();
     var $emergencyNumber = $("input[name=phone]").val();
     var url = "https://stageright.trail-staging.us/?campaign_id=2904&schedule=1&max_times_donate=3";
-
     var $assistance = $("#assistance").is(":checked")
-
     var $paymentOption = $("#payment_option").find(":selected").val();
-
-    var workshopTotal = calculateVoiceWorkshopTotal();
+    var homeSchoolTotal = calculateHomeSchoolDiscount();
+    var voiceTotal = calculateVoiceDiscount();
     var $workshopQty = $("select#voice-option-qty > option:selected").text() *1;
     var $workshopId = $("select#voice-option > option:selected").attr('value');
     var $workshopDescription = $("select#voice-option > option:selected").text();
 
-    if (workshopTotal > 0) {
+    if (homeSchoolTotal > 0) {
+      items ++;
+      url += "&cart[items]["+items+"][desc]=Home School";
+      url += "&cart[items]["+items+"][amount]="+homeSchoolTotal;
+      url += "&cart[items]["+items+"][product_id]=home_school";
+      url += "&cart[items]["+items+"][quantity]=1";
+    }
+
+    if (voiceTotal > 0) {
       items ++;
       url += "&cart[items]["+items+"][desc]="+$workshopDescription;
       url += "&cart[items]["+items+"][amount]="+workshopTotal;
       url += "&cart[items]["+items+"][product_id]="+$workshopId;
-      url += "&cart[items]["+items+"][quantity]="+$workshopQty;
+      url += "&cart[items]["+items+"][quantity]=1";
     }
 
     if ($paymentOption === "pay_in_full") {
       items ++;
-      url += "&cart[items]["+items+"][desc]=Child+Payment";
+      url += "&cart[items]["+items+"][desc]=Child Payment";
       url += "&cart[items]["+items+"][product_id]=pay_in_full";
     } else {
       items ++;
-      url += "&cart[items]["+items+"][desc]=Child+Deposit";
+      url += "&cart[items]["+items+"][desc]=Child Deposit";
       url += "&cart[items]["+items+"][product_id]=pay_deposit";
     };
-
-    if (name) {
-      items ++;
-      url += "&cart[items]["+items+"][notes]="+name;
-    }
 
     if ($assistance) {
       items ++;
@@ -81,14 +115,18 @@
       url += "&cart[items]["+items+"][quantity]=1";
     };
 
+    if ($name) {
+      items ++;
+      url += "&cart[items]["+items+"][notes]="+name;
+    }
+
     if ($emergencyNumber) {
       items ++;
-      url += "&cart[items]["+items+"][desc]="+$emergencyNumber;
-      url += "&cart[items]["+items+"][product_id]=emergency_number";
+      url += "&cart[items]["+items+"][notes]=emergency_number";
     }
 
     //window.location.href = url;
-    console.log(url);
+    alert(url);
   }
 
 })();
